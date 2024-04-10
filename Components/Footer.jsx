7bd9dde07e70affe5a0122adc121bf8ccc534e34
ritik zambre <you@example.com>
2024-04-10 16:46:19 +0530

    initial commit

---
+++ Components/Footer.jsx
@@ -0,0 +1,66 @@
+import { Fot1, Fot2 } from "../Components/index";
+
+export default () => {
+  const footerNavs = [
+    {
+      href: "javascript:void()",
+      name: "Terms",
+    },
+    {
+      href: "javascript:void()",
+      name: "License",
+    },
+    {
+      href: "javascript:void()",
+      name: "Privacy",
+    },
+    {
+      href: "javascript:void()",
+      name: "GitHub",
+    },
+  ];
+  return (
+    <footer className="pt-10">
+      <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
+        <div className="justify-between sm:flex">
+          <div className="space-y-6">
+            <img
+              src="https://www.svgrepo.com/show/303286/star-wars-logo.svg"
+              className="w-32"
+            />
+            <p className="max-w-md">
+              This web application is built on blockchain technology and serves
+              as a supply chain management tool. It enables users to track
+              shipments created through a smart contract, providing details such
+              as delivery location, time, payment status, and overall shipment
+              status.
+            </p>
+            <ul className="flex flex-wrap items-center gap-4 text-sm sm:text-base">
+              {footerNavs.map((item, idx) => (
+                <li className="text-gray-800 font-bold hover:text-blue-500 duration-150">
+                  <a key={idx} href={item.href}>
+                    {item.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="mt-6">
+            <p className="text-gray-700 font-semibold">Get the app</p>
+            <div className="flex items-center gap-3 mt-3 sm:block">
+              <a href="javascript:void()">
+                <Fot1 />
+              </a>
+              <a href="javascript:void()" className="mt-0 block sm:mt-3">
+                <Fot2 />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="mt-10 py-10 border-t md:text-center">
+          <p>© 2024 Ritik Zambre. All rights Un-reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
